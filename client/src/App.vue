80acadde561368f802d1c4fc213eb39111663173
khabubundivhu <khabubundivh@gmail.com>
2020-06-07 15:22:12 +0200

    :sparkles: Get audio feature

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,10 +5,6 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+
 
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,82 @@
 <template>
   <div class="home">
-    <Search />
+    <h1 class="text-center mt-3">Audio Feature Visualisation</h1>
+    <section class="search_box mt-5">
+      <div class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">Search for a music track to get it's features...</h5>
+        </div>
+        <div class="input-group mb-3 mt-3">
+          <input v-model.lazy="searchQuery" type="text" class="form-control" />
+        </div>
+      </div>
+    </section>
+    <div v-if="searchResults.length > 0" class="container mt-5">
+      <div class="row">
+        <div v-for="(track, index) in searchResults" :key="index" class="col-md-4" @click="audioFeature(track)">
+          <div class="card mb-3">
+            <div class="card-body">
+              <h5 class="card-title">{{ track.primary_artist }}</h5>
+              <h6 class="card-subtitle text-muted">{{ track.name }}</h6>
+            </div>
+            <img
+              style="height: 200px; width: 100%; display: block;"
+              :src="track.image"
+              alt="Card image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Search from '../components/Search'
 export default {
-  name: 'Home',
-  components: {
-    Search
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      searchQuery: "",
+      searchResults: []
+    };
+  },
+  watch: {
+    searchQuery: function(value) {
+      console.log(value);
+      this.searchResults = []
+      fetch(`http://localhost:5000/search?term=${value}`).then(res => {
+        res.json().then(data => {
+          data.tracks.items.map(track => {
+            const data = {
+              id: track.id,
+              name: track.name,
+              primary_artist: track.artists[0].name,
+              image: track.album.images[2].url
+            };
+            this.searchResults.push(data);
+          });
+        });
+      });
+    }
+  },
+  methods: {
+    audioFeature: function(track) {
+      fetch(`http://localhost:5000/features?id=${track.id}`).then(res => {
+            res.json().then(data => {
+                console.log(data)
+            });
+        })
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+.search_box {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
+}
+</style>
