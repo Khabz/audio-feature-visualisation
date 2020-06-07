7500c81de09db5a3aacdbd4363f0fc1a25a3a845
khabubundivhu <khabubundivh@gmail.com>
2020-06-07 19:38:59 +0200

    :sparkles: Ready for production

---
+++ client/src/components/Chart.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="modal fade"
+    id="staticBackdrop"
+    data-backdrop="static"
+    data-keyboard="false"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="staticBackdropLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">{{ selectedTrack.primary_artist }} - {{ selectedTrack.name }}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <column-chart
+            :data="[['danceability', features.danceability], ['energy', features.energy], 
+            ['speechiness', features.speechiness], ['acousticness', features.acousticness],
+            ['instrumentalness', features.instrumentalness], ['liveness', features.liveness],
+            ['valence', features.valence]]"
+            :colors="[['#6BD62C', '#FFDA35', '#FF5B32', '#D21FC3', '#1BD176', '#D2B21F', '#5DEA77']]"
+          ></column-chart>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["features", "selectedTrack"],
+  name: "Chart",
+  components: {}
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="home">
     <h1 class="text-center mt-3">Audio Feature Visualisation</h1>
-    <section class="search_box mt-5">
+    <section class="search_box mt-5 bg-primary">
       <div class="list-group-item list-group-item-action flex-column align-items-start">
         <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">Search for a music track to get it's features...</h5>
+          <h5 class="mb-1 text-center">Search for a music track to get it's features...</h5>
         </div>
         <div class="input-group mb-3 mt-3">
           <input v-model.lazy="searchQuery" type="text" class="form-control" />
@@ -13,13 +13,19 @@
     </section>
     <div v-if="searchResults.length > 0" class="container mt-5">
       <div class="row">
-        <div v-for="(track, index) in searchResults" :key="index" class="col-md-4" @click="audioFeature(track)">
+        <div
+          v-for="(track, index) in searchResults"
+          :key="index"
+          class="col-md-4"
+          @click="audioFeature(track)"
+        >
           <div class="card mb-3">
             <div class="card-body">
               <h5 class="card-title">{{ track.primary_artist }}</h5>
               <h6 class="card-subtitle text-muted">{{ track.name }}</h6>
             </div>
             <img
+              class="text-center"
               style="height: 200px; width: 100%; display: block;"
               :src="track.image"
               alt="Card image"
@@ -28,24 +34,38 @@
         </div>
       </div>
     </div>
+    <Chart :features="features" :selectedTrack="selectedTrack"  />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Chart from '../components/Chart'
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Chart
+  },
   data() {
     return {
       searchQuery: "",
-      searchResults: []
+      searchResults: [],
+      features: {
+        danceability:null,
+        energy: null,
+        speechiness: null,
+        acousticness: null,
+        instrumentalness: null,
+        liveness: null,
+        valence: null
+      },
+      selectedTrack: null
     };
   },
   watch: {
     searchQuery: function(value) {
       console.log(value);
-      this.searchResults = []
+      this.searchResults = [];
       fetch(`http://localhost:5000/search?term=${value}`).then(res => {
         res.json().then(data => {
           data.tracks.items.map(track => {
@@ -63,11 +83,21 @@ export default {
   },
   methods: {
     audioFeature: function(track) {
+      this.selectedTrack = track
       fetch(`http://localhost:5000/features?id=${track.id}`).then(res => {
-            res.json().then(data => {
-                console.log(data)
-            });
-        })
+        res.json().then(data => {
+          console.log(data)
+          this.features.danceability = data.danceability
+          this.features.energy = data.energy
+          this.features.speechiness = data.speechiness
+          this.features.acousticness = data.acousticness
+          this.features.instrumentalness = data.instrumentalness
+          this.features.liveness = data.liveness
+          this.features.valence = data.valence
+          console.log("I want to open a modal, But this fucker is refusing")
+          window.$("#staticBackdrop").modal("show")
+        });
+      });
     }
   }
 };
